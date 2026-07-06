39a1f29cbf04562ed87f5a76ebcd931826492dc7
kmctwebdev1 <webdev1@kmct.org>
2026-07-06 12:36:33 +0530

    backend setup

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+// Initialized app.js
+import express from 'express';
+const app = express();
+
+// Middlewares
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+export default app;
