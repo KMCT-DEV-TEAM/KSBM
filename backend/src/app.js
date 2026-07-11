6bd50621a3f70e884376d0f69e737427869d1fd2
kmctwebdev1 <webdev1@kmct.org>
2026-07-11 12:59:19 +0530

    converted to next.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,7 @@ dotenv.config();
 
 // Middlewares   
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001", "https://ksbm-1.onrender.com"],
+  origin: ["http://localhost:3000", "http://localhost:3001", "https://ksbm-2.onrender.com"],
   credentials: true,
 }));
 app.use(express.json());
