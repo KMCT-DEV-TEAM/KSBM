9841c523a4e65d5d78e1a24b4a65b27196cfde7c
kmctwebdev1 <webdev1@kmct.org>
2026-07-13 10:04:40 +0530

    hosting done

--- frontend/.next/types/app/layout.ts
+++ frontend/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: D:\KMCT\KSBM\frontend\src\app\layout.jsx
+// File: D:\KMCT\Projects\KSBM\frontend\src\app\layout.jsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
